b0a412a65df3157b2cc9fef30f85e252af11fa06
stmejia <rudymejia.pro@gmail.com>
2022-03-07 23:59:42 -0600

    Fix Cors

--- Startup.cs
+++ Startup.cs
@@ -53,6 +53,8 @@ namespace CamioncitoAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors("AllowOrigin");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
